c8b9d79dc01693a1e2e8fa410ef151873d8ff07e
ShannonTao <shannon.cxq@gmail.com>
2022-07-14 17:31:32 -0400

    troubleshooting event listener

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ pokeApp.ulEl = document.querySelector(".displayPokemon");
 
 pokeApp.pokeCards = [];
 pokeApp.firstChoiceSelected = false;
-pokeApp.matching = false;
 pokeApp.counter = 0;
 
+// Fetch Pokemon Data using API, add url and name into an array
 pokeApp.fetchData = () => {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=5&offset=0")
     .then(function (data) {
@@ -27,6 +27,7 @@ pokeApp.fetchData = () => {
     });
 };
 
+// fetch ANOTHER set of data using the URL we just received from the first API
 pokeApp.fetchImages = function (pokeCards) {
   pokeCards.forEach((card) => {
     fetch(card.url)
@@ -49,69 +50,53 @@ pokeApp.fetchImages = function (pokeCards) {
         pokeApp.newLi.append(pokeApp.frontDiv, pokeApp.backDiv);
         pokeApp.ulEl.appendChild(pokeApp.newLi);
 
+        // call function for eventListener
         pokeApp.addClickSetup();
       });
   });
 };
 
-pokeApp.checkMatch = () => {
-  console.log("heading to check match");
-  if (pokeApp.matching) {
-    pokeApp.removeClickSetup();
-    console.log("matching?");
-  }
-
-  if (
-    pokeApp.firstChoiceSelected &&
-    pokeApp.secondChoice.alt === pokeApp.firstChoice.alt
-  ) {
-    console.log("it's a match!");
-    pokeApp.firstChoiceSelected = false;
-    setTimeout(function () {
-      pokeApp.firstChoice.remove();
-      pokeApp.secondChoice.remove();
-    }, 2000);
-  } else {
-    setTimeout(function () {
-      pokeApp.cardImageOne.classList.toggle("hide");
-      pokeApp.cardImageTwo.classList.toggle("hide");
-    }, 2000);
-    pokeApp.firstChoiceSelected = false;
-  }
-  pokeApp.matching = false;
-};
-
+// setup Event Listener
 pokeApp.addClickSetup = () => {
   pokeApp.newLi.addEventListener("click", function (e) {
     pokeApp.handleClick(e);
   });
 };
 
-pokeApp.removeClickSetup = () => {
-  pokeApp.newLi.removeEventListener("click", pokeApp.handleClick());
-};
-
+// When user's click, execute line of code
 pokeApp.handleClick = (e) => {
   //counter to keep trash of clicks
 
+  // adds counter
   pokeApp.counter++;
+
+  // If counter is less than or equal to two, grab the elements and store in variables
   if (pokeApp.counter <= 2) {
+    // If first choice selected is false, grab the elements and store in variables
     if (!pokeApp.firstChoiceSelected) {
-      console.log(e.target.parentNode.nextSibling.firstChild.alt);
       pokeApp.firstChoice = e.target.parentNode.nextSibling.firstChild;
 
+      // store card image parent variable into new variable
       pokeApp.cardImageOne = e.target.parentNode;
+      console.log(pokeApp.cardImageOne);
 
       pokeApp.cardImageOne.classList.toggle("hide");
 
       pokeApp.firstChoiceSelected = true;
     } else {
+      // ELSE, grab the elements and store in SECOND choice variables
+
       pokeApp.secondChoice = e.target.parentNode.nextSibling.firstChild;
+
+      // store card image parent variable into new variable
       pokeApp.cardImageTwo = e.target.parentNode;
+      console.log(pokeApp.cardImageTwo);
+
       pokeApp.cardImageTwo.classList.toggle("hide");
     }
     console.log(pokeApp.counter);
   } else {
+    // if counter has reached two, remove event listener so users cannot click while we are checking if the two choices match
     pokeApp.newLi.removeEventListener("click", pokeApp.handleClick);
   }
 
@@ -121,15 +106,43 @@ pokeApp.handleClick = (e) => {
   ) {
     pokeApp.checkMatch();
   } else {
-    console.log("pick another card");
+  }
+
+  pokeApp.addClickSetup();
+
+  console.log(pokeApp.firstChoice);
+  console.log(pokeApp.secondChoice);
+};
+
+// checks Pokemon Match
+pokeApp.checkMatch = () => {
+  if (
+    pokeApp.firstChoiceSelected &&
+    pokeApp.secondChoice.alt === pokeApp.firstChoice.alt
+  ) {
+    // IF the Value of the First choice and second choice is the same, it will remove the image from the page
+
+    setTimeout(function () {
+      pokeApp.firstChoice.remove();
+      pokeApp.secondChoice.remove();
+      pokeApp.counter = 0;
+      // reset first choice back to false
+      pokeApp.firstChoiceSelected = false;
+    }, 2000);
+  } else {
+    // If value of first and second choice do NOT match, flip the cards back over
+    setTimeout(function () {
+      pokeApp.cardImageOne.classList.toggle("hide");
+      pokeApp.cardImageTwo.classList.toggle("hide");
+      pokeApp.counter = 0;
+      // reset first choice back to false
+      pokeApp.firstChoiceSelected = false;
+    }, 2000);
   }
 };
 
 pokeApp.init = () => {
-  console.log("ready to go!");
   pokeApp.fetchData();
-
-  console.log("the results");
 };
 
 pokeApp.init();

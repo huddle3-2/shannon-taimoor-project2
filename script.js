5548cf8f01b3a20365e7bacb2c3e02b0c3f82997
Taimoor <taimoor.m89@gmail.com>
2022-07-14 17:05:34 -0400

    prevent users from clicking while matching

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ pokeApp.ulEl = document.querySelector(".displayPokemon");
 
 pokeApp.pokeCards = [];
 pokeApp.firstChoiceSelected = false;
+pokeApp.matching = false;
+pokeApp.counter = 0;
 
 pokeApp.fetchData = () => {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=5&offset=0")
@@ -35,7 +37,7 @@ pokeApp.fetchImages = function (pokeCards) {
         card.urlImg = result.sprites.front_default;
       })
       .then(function () {
-        const newLi = document.createElement("li");
+        pokeApp.newLi = document.createElement("li");
         pokeApp.backDiv = document.createElement("div");
         pokeApp.frontDiv = document.createElement("div");
 
@@ -44,51 +46,82 @@ pokeApp.fetchImages = function (pokeCards) {
 
         pokeApp.frontDiv.innerHTML = `<img src="./assets/back-card.png" draggable="false"/>`;
         pokeApp.backDiv.innerHTML = `<img src=${card.urlImg} alt=${card.name}>`;
-        newLi.append(pokeApp.frontDiv, pokeApp.backDiv);
-        pokeApp.ulEl.appendChild(newLi);
+        pokeApp.newLi.append(pokeApp.frontDiv, pokeApp.backDiv);
+        pokeApp.ulEl.appendChild(pokeApp.newLi);
 
-        newLi.addEventListener("click", function (e) {
-          if (!pokeApp.firstChoiceSelected) {
-            console.log(e.target.parentNode.nextSibling.firstChild.alt);
-            pokeApp.firstChoice = e.target.parentNode.nextSibling.firstChild;
-
-            pokeApp.cardImage = e.target.parentNode;
-
-            pokeApp.cardImage.classList.add("hide");
-
-            pokeApp.firstChoiceSelected = true;
-          } else {
-            pokeApp.secondChoice = e.target.parentNode.nextSibling.firstChild;
-
-            pokeApp.cardImage.classList.add("hide");
-          }
-
-          if (
-            pokeApp.firstChoice &&
-            pokeApp.firstChoice !== e.target.parentNode.nextSibling.firstChild
-          ) {
-            pokeApp.checkMatch();
-          } else {
-            console.log("pick another card");
-          }
-        });
+        pokeApp.addClickSetup();
       });
   });
 };
 
 pokeApp.checkMatch = () => {
   console.log("heading to check match");
+  if (pokeApp.matching) {
+    pokeApp.removeClickSetup();
+    console.log("matching?");
+  }
 
   if (
     pokeApp.firstChoiceSelected &&
     pokeApp.secondChoice.alt === pokeApp.firstChoice.alt
   ) {
     console.log("it's a match!");
-    pokeApp.frontDiv.remove();
-    pokeApp.backDiv.remove();
     pokeApp.firstChoiceSelected = false;
+    setTimeout(function () {
+      pokeApp.firstChoice.remove();
+      pokeApp.secondChoice.remove();
+    }, 2000);
   } else {
-    pokeApp.cardImage.classList.add("show");
+    setTimeout(function () {
+      pokeApp.cardImageOne.classList.toggle("hide");
+      pokeApp.cardImageTwo.classList.toggle("hide");
+    }, 2000);
+    pokeApp.firstChoiceSelected = false;
+  }
+  pokeApp.matching = false;
+};
+
+pokeApp.addClickSetup = () => {
+  pokeApp.newLi.addEventListener("click", function (e) {
+    pokeApp.handleClick(e);
+  });
+};
+
+pokeApp.removeClickSetup = () => {
+  pokeApp.newLi.removeEventListener("click", pokeApp.handleClick());
+};
+
+pokeApp.handleClick = (e) => {
+  //counter to keep trash of clicks
+
+  pokeApp.counter++;
+  if (pokeApp.counter <= 2) {
+    if (!pokeApp.firstChoiceSelected) {
+      console.log(e.target.parentNode.nextSibling.firstChild.alt);
+      pokeApp.firstChoice = e.target.parentNode.nextSibling.firstChild;
+
+      pokeApp.cardImageOne = e.target.parentNode;
+
+      pokeApp.cardImageOne.classList.toggle("hide");
+
+      pokeApp.firstChoiceSelected = true;
+    } else {
+      pokeApp.secondChoice = e.target.parentNode.nextSibling.firstChild;
+      pokeApp.cardImageTwo = e.target.parentNode;
+      pokeApp.cardImageTwo.classList.toggle("hide");
+    }
+    console.log(pokeApp.counter);
+  } else {
+    pokeApp.newLi.removeEventListener("click", pokeApp.handleClick);
+  }
+
+  if (
+    pokeApp.firstChoice &&
+    pokeApp.firstChoice !== e.target.parentNode.nextSibling.firstChild
+  ) {
+    pokeApp.checkMatch();
+  } else {
+    console.log("pick another card");
   }
 };
 
